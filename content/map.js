ef8a2d9a80a9648220eb092ddbdb7600b6351f08
Joey <1055157299@qq.com>
2024-04-05 21:11:08 +0800

    2

--- content/map.js
+++
@@ -1,7 +0,0 @@
-// map.js
-var map = L.map('map').setView([51.505, -0.09], 13); // 设置中心点和缩放级别
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map); // 添加标记
